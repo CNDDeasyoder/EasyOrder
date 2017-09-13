238d058e93ce47ce93b3845ef4b88eb1c9f911ef
tonqthonq97 <tonqthonq97@gmail.com>
2017-09-13 13:00:38 +0700

    Initial commit

---
+++ app/src/main/java/kesu/easyorder/MainActivity.java
@@ -0,0 +1,13 @@
+package kesu.easyorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_bill);
+    }
+}
